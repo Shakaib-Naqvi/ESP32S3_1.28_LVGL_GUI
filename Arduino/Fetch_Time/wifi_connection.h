5803937fd6f84c58d2d0e31fba0a744081213d01
Shakaib Naqvi <55704107+Shakaib-Naqvi@users.noreply.github.com>
2026-02-12 19:11:17 +0500

    Code added to fetch time from internet

---
+++ Arduino/Fetch_Time/wifi_connection.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_CONNECTION_H
+#define WIFI_CONNECTION_H
+
+const char* ssid     = "eSpark Consulting";
+const char* password = "47356880";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 3600;
+#endif
